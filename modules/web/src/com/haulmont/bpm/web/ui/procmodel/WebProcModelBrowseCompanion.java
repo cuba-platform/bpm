5bf855aa7df6ea27064d62ac0b323272cdf1c5c6
Daniil Tsarev <tsarev@haulmont.com>
2016-03-25 17:14:23 +0400

    PL-6383 Workflow designer should be opened at the same address as the main program

--- modules/web/src/com/haulmont/bpm/web/ui/procmodel/WebProcModelBrowseCompanion.java
+++ modules/web/src/com/haulmont/bpm/web/ui/procmodel/WebProcModelBrowseCompanion.java
@@ -5,11 +5,10 @@
 
 package com.haulmont.bpm.web.ui.procmodel;
 
+import com.haulmont.bali.util.ParamsMap;
 import com.haulmont.bpm.gui.procmodel.ProcModelBrowse;
 import com.haulmont.cuba.web.App;
 import com.haulmont.cuba.web.controllers.ControllerUtils;
-
-import java.util.Collections;
 
 /**
  */
@@ -19,6 +18,6 @@ public class WebProcModelBrowseCompanion implements ProcModelBrowse.Companion {
     public void openModeler(String modelerUrl) {
         String webAppUrl = ControllerUtils.getLocationWithoutParams();
         String url = webAppUrl + modelerUrl;
-        App.getInstance().getWindowManager().showWebPage(url, Collections.<String, Object>singletonMap("tryToOpenAsPopup", Boolean.TRUE));
+        App.getInstance().getWindowManager().showWebPage(url, ParamsMap.of("tryToOpenAsPopup", Boolean.TRUE));
     }
 }
