5cf99f785e646732660cec25c52c151a37aa5fe7
Maxim Gorbunkov <gorbunkov@haulmont.com>
2018-01-11 18:15:52 +0400

    PL-10231 After upgrading to 6.7.7 studio suggests to alter BPM_PROC_INSTANCE table - mysql script fixed

--- modules/core/db/update/mysql/18/180111-removeIntEntityIdColumnsFromProcInstance.sql
+++ modules/core/db/update/mysql/18/180111-removeIntEntityIdColumnsFromProcInstance.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE cuba_drop_column_if_exists() BEGIN
-IF EXISTS ( SELECT * FROM information_schema.columns WHERE table_name = 'BPM_PROC_INSTANCE' AND column_name = 'STRING_ENTITY_ID' AND table_schema = DATABASE() )
+IF EXISTS ( SELECT * FROM information_schema.columns WHERE table_name = 'BPM_PROC_INSTANCE' AND column_name = 'INT_ENTITY_ID' AND table_schema = DATABASE() )
 then
-    ALTER TABLE BPM_PROC_INSTANCE DROP COLUMN STRING_ENTITY_ID;
+    ALTER TABLE BPM_PROC_INSTANCE DROP COLUMN INT_ENTITY_ID;
 end if;
 end;
 CALL cuba_drop_column_if_exists();
